225c1f0b18bc91c35d77a24fde3897e334d2ca03
DaviCDS301 <davicds1604@gmail.com>
2025-11-25 17:26:18 -0300

    projeto feito junto com o professor sobre javascript dom

---
+++ Projeto/javascrip.js
@@ -0,0 +1,44 @@
+const titulo = document.getElementById("titulo");
+const btnTrocarTexto = document.getElementById("btnTrocarTexto");
+
+btnTrocarTexto.addEventListener("click", function(){
+    titulo.textContent = "O título foi alterado via DOM";
+} );
+
+const descricoes = document.getElementsByClassName("descricao");
+const btnDestaque = document.getElementById("btnDestaque");
+
+btnDestaque.addEventListener("click", function() {
+    for (let item of descricoes){
+        item.classList.toggle("highlight");
+    }
+} );
+
+const btnQuery = document.getElementById("btnQuery");
+
+btnQuery.addEventListener("click", function(){
+    const primeiroParagrafo = document.querySelector("#conteiner p")
+    primeiroParagrafo.style.color = "red";
+} );
+
+const btnQueryAll = document.getElementById("btnQueryAll");
+
+btnQueryAll.addEventListener("click", function() {
+    const paragrafo = document.querySelectorAll("#conteiner p");
+
+    paragrafo.forEach( p => {
+        p.style.border = "1px solid black";
+        p.style.margin = "4px 0";
+    });
+} );
+
+const btnCriar = document.getElementById("btnCriar");
+
+btnCriar.addEventListener("click", function() {
+    const novo = document.createElement("p");
+
+    novo.textContent = "Eu fui criado dinamicamente";
+    novo.classList.add("descricao");
+
+    document.getElementById("conteiner").appendChild(novo);
+} );
