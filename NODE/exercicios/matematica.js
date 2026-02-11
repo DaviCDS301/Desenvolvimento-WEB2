e7d033efa1b16c2b071e3f92832a8c1d312d1cdf
DaviCDS301 <Davi_C_Dos-Santos@estudante.sesisenai.org.br>
2026-02-11 15:46:49 -0300

    exercicio dois feito.

---
+++ NODE/exercicios/matematica.js
@@ -0,0 +1,23 @@
+let num1 = 7;
+let num2 = 4;
+let resultado;
+
+console.log("numero um: ", num1);
+
+console.log("numero dois: ", num2);
+
+resultado = num1 + num2;
+
+console.log("soma:", resultado);
+
+resultado = num1 - num2;
+
+console.log("subtração:", resultado);
+
+resultado = num1 * num2;
+
+console.log("multipkicação:", resultado);
+
+resultado = num1 / num2;
+
+console.log("divisão:", resultado);
