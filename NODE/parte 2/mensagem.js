94fd00d4c897b55066a6bf1997a856bbf773fb3b
DaviCDS301 <Davi_C_Dos-Santos@estudante.sesisenai.org.br>
2026-02-12 14:09:25 -0300

    exercicio dois da parte dois feito

---
+++ NODE/parte 2/mensagem.js
@@ -0,0 +1,17 @@
+function boasVindas(nome){
+    return nome;
+}
+
+function despedidas(name){
+    return name;
+}
+
+function autorSistema(autor){
+    return autor;
+}
+
+module.exports = {
+    boasVindas,
+    despedidas,
+    autorSistema
+};
---
+++ NODE/parte 2/receptor.js
@@ -0,0 +1,7 @@
+const mensagem = require("./mensagem");
+
+console.log("Bem vindo, ", mensagem.boasVindas("jupiro"));
+
+console.log("Tchau, ", mensagem.despedidas("jupiro"));
+
+console.log("Autor do Sistema: ", mensagem.autorSistema("davi"));
