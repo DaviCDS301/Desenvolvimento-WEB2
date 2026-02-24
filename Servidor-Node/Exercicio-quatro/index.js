fe880ce5760e75b138d8434b0a2574578709e99f
DaviCDS301 <Davi_C_Dos-Santos@estudante.sesisenai.org.br>
2026-02-24 14:58:34 -0300

    exercício quatro feito!

---
+++ Servidor-Node/Exercicio-quatro/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+
+const servidor = http.createServer((req, res) => {
+
+    fs.readFile('index.html', (erro, dados) => {
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(dados);
+
+    });
+
+});
+
+servidor.listen(3000);
