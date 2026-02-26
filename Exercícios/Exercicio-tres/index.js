4e599d959dd380095fdc2809204dc5f8a227199a
DaviCDS301 <Davi_C_Dos-Santos@estudante.sesisenai.org.br>
2026-02-26 14:44:48 -0300

    Exercício três feito

---
+++ Exercícios/Exercicio-tres/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const produtos = [];
+
+app.get('/produtos', (req, res) => {
+  res.json(produtos);
+});
+
+app.post('/produtos', (req, res) => {
+
+    const produto = req.body;
+  
+    produtos.push(produto);
+  
+    res.status(201).json(produto);
+  
+  });
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
