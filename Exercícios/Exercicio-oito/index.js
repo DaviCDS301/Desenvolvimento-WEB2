c1bbaca82278bb75c9784a8858cf2485c5cf014b
DaviCDS301 <Davi_C_Dos-Santos@estudante.sesisenai.org.br>
2026-02-26 17:20:45 -0300

    exercício oito corrigido de novo

--- Exercícios/Exercicio-oito/index.js
+++ Exercícios/Exercicio-oito/index.js
@@ -6,9 +6,9 @@ app.use(express.json());
 app.use(cors());
 
 const produtos = [
-    {id: 1, nome: "Harry Potter e o Cálice de fogo", preco: 75},
-    {id: 2, nome: "Stranger Things - Cidade nas Trevas", preco: 20},
-    {id: 3, nome: "Star Wars - Trilogy", preco: 35}
+    {id: 1, nome: "Harry Potter e o Cálice de fogo", autor: "J.S.Rolling", ano: 2000},
+    {id: 2, nome: "Stranger Things - Cidade nas Trevas", autor: "Adam Christopher", ano: 2020},
+    {id: 3, nome: "Star Wars - Trilogy", autor: "George Lucas", ano: 2014}
 ];
 
 app.get("/produtos", (req, res) => {
