6ceaea44f6587925cc525cf8a267ff9aec0f9d37
DaviCDS301 <Davi_C_Dos-Santos@estudante.sesisenai.org.br>
2026-02-26 17:10:42 -0300

    Exercício oito feito

---
+++ Exercícios/Exercicio-oito/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const produtos = [
+    {id: 1, nome: "Pudim", preco: 75},
+    {id: 2, nome: "Pão", preco: 20},
+    {id: 3, nome: "salgadinho", preco: 35}
+];
+
+app.get("/produtos", (req, res) => {
+    res.json(produtos);
+});
+
+app.get('/produtos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const produto = produtos.find(p => p.id === id);
+  
+    if (!produto) {
+      return res.status(404).send("Produto não encontrado");
+    }
+  
+    res.json(produto);
+});
+
+app.delete("/produtos/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    const index = produtos.findIndex(produto => produto.id === id);
+  
+    if (index === -1) {
+      return res.status(404).json({ mensagem: "Produto não encontrado" });
+    }
+  
+    produtos.splice(index, 1);
+  
+    res.json({ mensagem: "Produto removido com sucesso" });
+  });
+
+app.put("/produtos/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const novosDados = req.body;
+  
+    const index = produtos.findIndex(p => p.id === id);
+  
+    if (index === -1) {
+      return res.status(404).json({ mensagem: "Produto não encontrado" });
+    }
+  
+    produtos[index] = {
+      ...produtos[index],
+      ...novosDados
+    };
+  
+    res.json(produtos[index]);
+});
+
+app.post('/produtos', (req, res) => {
+
+    const produto = req.body;
+  
+    produtos.push(produto);
+  
+    res.status(201).json(produto);
+  
+  });
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
